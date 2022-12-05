a8e5a0cad33199220af74441b37c505be64b9263
czehr22 <zehr0025@umn.edu>
2022-12-05 11:56:42 -0600

    12/5/2022

---
+++ Flywheel calcs/getDeltaKE.m
@@ -0,0 +1,36 @@
+
+function [ deltaKE  ]  = getDeltaKE( theta0, thetaF, Tavg, torque, theta2)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  FUNCTION NAME: getDeltaKE
+%
+%  PURPOSE: Calculate the change in kinetic energy from the torque response
+%  of the system
+%
+%  INPUT: torque, theta0, thetaF, theta2, Tavg
+%
+%  OUTPUT: deltaKE (J)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%  AUTHOR: Carter Zehr
+%  DATE: 12/5/2022
+%
+%  DESCRIPTION OF LOCAL VARIABLES
+%
+%  FUNCTIONS CALLED
+%   trapz
+%
+%  START OF EXECUTABLE CODE
+%
+
+%set bounds of integration
+theta0check = theta2 - theta0;
+thetaFcheck = theta2 - thetaF;
+
+[minimum,theta0index] = min(abs(theta0check));
+[minimum,thetaFindex] = min(abs(thetaFcheck));
+
+bounds = theta2(theta0index:thetaFindex);
+deltaKE = trapz(bounds, torque(theta0index:thetaFindex)-Tavg);
+
+end
