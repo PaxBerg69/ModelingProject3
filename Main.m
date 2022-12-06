23134b966ba73af66e8dd50ce214c33415c6f419
Trey <webe0988@umn.edu>
2022-12-06 11:11:02 -0600

    test

--- Main.m
+++ Main.m
@@ -83,7 +83,7 @@ Tavg = getTavg(theta2, torque);
 deltaKE = getDeltaKE(theta0, thetaF, Tavg, torque, theta2);
 I = getI(deltaKE,Cf,omega_avg);
 [flywheelDiaO] = getFlywheelsize(I);
-w_2 = getOmega(Tavg,I,torque,theta0);
+w_2 = getOmega(Tavg,I,torque,theta2);
 % printOutput(theta2,theta0,torque,power,flywheelDiaO,P,volumeT,w_2,Pbot,Ptop, P1, P2, P3, P4);
 
 %%Parameter Vary
