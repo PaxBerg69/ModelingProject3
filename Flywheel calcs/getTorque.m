2f32f75be1c7bfed6b71dedf3c5cfa92c83950f9
czehr22 <zehr0025@umn.edu>
2022-12-06 11:01:59 -0600

    12/6/2022

--- Flywheel calcs/getTorque.m
+++ Flywheel calcs/getTorque.m
@@ -1,15 +1,5 @@
-% DELETE COMMENTED PORTION IF ALL THESE ARE DEFINED ELSEWHERE
 
-% Fp = linspace(0,500,3600);
-% torque = zeros(1,3600);
-% theta2 = linspace(0.1,360,3600);
-% length.OaA = 0.0138;
-% torque = getTorquetest(Fp, length, theta2);
-% 
-% plot(theta2, torque);
-
-
-function [torque]  = getTorque( Fp, length, theta2 )
+function [torque]  = getTorque( Fp, length, theta2, torque, theta3power )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  FUNCTION NAME: getTorque
 %
@@ -33,4 +23,23 @@ function [torque]  = getTorque( Fp, length, theta2 )
 %  START OF EXECUTABLE CODE
 %
 
-torque = -Fp .* cosd(theta2 - 90.0) .* length.OaA;
+length.AG3 = length.AB / 2.0;
+length.BG3 = length.AB / 2.0;
+
+F12x = zeros(1,3600);
+F12y = zeros(1,3600);
+F23x = zeros(1,3600);
+F23y = zeros(1,3600);
+F34x = zeros(1,3600);
+F34y = zeros(1,3600);
+
+for z = 1:3600
+    A = [1 0 -1 0 0 0 0; 0 1 0 -1 0 0 0; 0 0 length.OaA*sind(theta2(z)) -length.OaA*cosd(theta2(z)) 0 0 1; 0 0 1 0 -1 0 0; 0 0 0 1 0 -1 0; 0 0 -length.AG3*sind(theta3power(z)-180) length.AG3*cosd(theta3power(z)-180) length.BG3*sind(theta3power(z)) -length.BG3*cosd(theta3power(z)) 0; 0 0 0 0 0 1 0];  %matrix with coefficients in front of each force/torque
+    C = [0; 0; 0; 0; 0; 0; Fp(z)];   %matrix relating forces to weights and inertial forces/torques
+
+    B = inv(A)*C;   %from linear algrebra - B matrix will fill with relevant force values
+
+    torque(z) = -B(7);    %establish each force/torque component according to matrix setup (see pdf for list of the 7 equations used)
+end
+
+plot(theta2, torque);
