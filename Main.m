c3803cc0ac785d19e2ca96bba8038e8b7b059979
PaxBerg69 <117890057+PaxBerg69@users.noreply.github.com>
2022-12-07 15:03:26 -0600

    fix

--- Main.m
+++ Main.m
@@ -90,7 +90,6 @@ Thigh = linspace(400,2000,plotResolution);
 powerV = zeros(1,plotResolution);
 Dvary = zeros(1,plotResolution);
 for j = 1:plotResolution
-<<<<<<< HEAD
     Te = Thigh(j);
     [theta3displacer, theta3power] = getTheta3(length,theta2);
     [ydisplacer, ypower ]  = getYPosition( theta2, theta3displacer, theta3power, length);
@@ -98,7 +97,7 @@ for j = 1:plotResolution
     [volumeC] = getVolumeC(ydisplacer, ypower, cylD);
     volumeT = volumeE+volumeC;
     [P] = getPressure(Pmin,volumeC,volumeE,volumeR,Tc,Te,theta2);
-    [ P1, P2, P3, P4, Ptop, Pbot ]  = getIdeal( P, volumeC, volumeR, volumeE, Pbot, Ptop, Pmin );
+    [ P1, P2, P3, P4, Ptop, Pbot ]  = getIdeal(volumeC, volumeR, volumeE, Pmin );
     Fp = getFp(P);
     [torque] = getTorque(Fp,length,theta2, torque, theta3power);
     Tavg = getTavg(theta2, torque);
@@ -109,25 +108,6 @@ for j = 1:plotResolution
     w_2 = getOmega(Tavg,I,torque,theta2);
     powerV(j) = Tavg*omega_avg/1000;
     [COF_act,w_2] = getOmega(Tavg,I,torque,theta2);
-=======
-Te = Thigh(j);
-[theta3displacer, theta3power] = getTheta3(length,theta2);
-[ydisplacer, ypower ]  = getYPosition( theta2, theta3displacer, theta3power, length);
-[volumeE] = getVolumeE(cylD,ydisplacer);
-[volumeC] = getVolumeC(ydisplacer, ypower, cylD);
-volumeT = volumeE+volumeC;
-[P] = getPressure(Pmin,volumeC,volumeE,volumeR,Tc,Te,theta2);
-[ P1, P2, P3, P4, Ptop, Pbot ]  = getIdeal( volumeC, volumeR, volumeE, Pmin );
-Fp = getFp(P);
-[torque] = getTorque(Fp,length,theta2, torque, theta3power);
-Tavg = getTavg(theta2, torque);
-[theta0, thetaF] = getThetas(torque, Tavg);
-deltaKE = getDeltaKE(theta0, thetaF, Tavg, torque, theta2);
-I = getI(deltaKE,Cf,omega_avg);
-Dvary(j) = 1000*getFlywheelsize(I);
-powerV(j) = Tavg*omega_avg/1000;
-[COF_act,w_2] = getOmega(Tavg,I,torque,theta2);
->>>>>>> 696e184b0a932e4d82cef5a2adfd715b3f5cf95d
 end
 hold off
 
